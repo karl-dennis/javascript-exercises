38983fa9fdc40962f730a1dcf33424db25e1077a
KarlDennis <karlgaspan0412@gmail.com>
2024-07-15 21:45:15 +0800

    done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  convert = (temperature - 32) * 5/9
+  return parseFloat(convert.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  convert = (9/5 * temperature) + 32
+  return parseFloat(convert.toFixed(1))
 };
 
 // Do not edit below this line
